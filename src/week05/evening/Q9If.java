de41fdf488d22e734ff04a288325b5e5b24532cc
Kemail-terminal <sulkemref@gmail.com>
2023-08-02 11:59:29 -0500

    week05_evening_Question_9

---
+++ src/week05/evening/Q9If.java
@@ -0,0 +1,23 @@
+package week05.evening;
+
+public class Q9If {
+
+    public static void main(String[] args) {
+
+        int a=10;
+        System.out.println(a++); //printing // if it is post increment first do your job for same statement so print 10
+        System.out.println(a); //11
+
+        int b=10;
+
+        System.out.println(++b); //if it is pre increment immediately increment your value and then you can do your
+        // job  //print 11
+
+        System.out.println(b); //b =11
+
+
+
+
+
+    }
+}
